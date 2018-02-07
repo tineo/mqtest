5405b867d0f722370a9b8a11029cc2a24edcd5d6
Cesar Gutierrez <tineo@dgt.dgt-domain>
2018-02-07 11:55:35 -0500

    first commit

---
+++ config.php
@@ -0,0 +1,12 @@
+<?php
+
+require_once __DIR__ . '/vendor/autoload.php';
+
+define('HOST', '104.131.75.179');
+define('PORT', 5672);
+define('USER', 'tineo');
+define('PASS', 'tineo');
+define('VHOST', '/');
+
+//If this is enabled you can see AMQP output on the CLI
+define('AMQP_DEBUG', true);
